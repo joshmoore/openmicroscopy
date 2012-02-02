140759a20994136535b44a55acd6fb1d8b96447a
Simon Wells <szwells@dundee.ac.uk>
2012-02-02 15:12:42 +0000

    updated key file name & chmod 0600. Fixes #7974

--- docs/install/VM/connect.sh
+++ docs/install/VM/connect.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 # This script will shell into an OMERO.VM
 
-KEY="omerokey"
+KEY="omerovmkey"
 USER="omero"
 HOST="localhost"
 PORT="2222"
 OPTIONS="StrictHostKeyChecking=no"
+
+chmod 0600 omerovmkey
 
 ssh -o $OPTIONS -i $KEY -p $PORT -t $USER@$HOST
