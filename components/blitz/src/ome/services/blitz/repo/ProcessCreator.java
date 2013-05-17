a2915996df81e04f944e1fb0b541f4d55d01e48d
jmoore <josh@glencoesoftware.com>
2013-05-17 10:28:08 +0200

    Initial version of ProcessCreator
    This requires other changes from the refactoring, but with those
    the unit tests now successfully pass, so saving.

---
+++ components/blitz/src/ome/services/blitz/repo/ProcessCreator.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright (C) 2013 Glencoe Software, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package ome.services.blitz.repo;
+
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ome.services.blitz.repo.path.FsFile;
+import omero.ServerError;
+import omero.sys.EventContext;
+
+import org.apache.commons.lang.text.StrLookup;
+import org.apache.commons.lang.text.StrSubstitutor;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import Ice.Current;
+
+/**
+ * Extensino point which can be modified and injected into
+ * {@link ManagedRepositoryI} instances to change which
+ * {@link ProcessContainer.Process} is created. By default, a
+ * {@link ManagedImportProcessI} will be created.
+ *
+ * @author Colin Blackburn <cblackburn at dundee dot ac dot uk>
+ * @author Josh Moore, josh at glencoesoftware.com
+ * @author m.t.b.carroll@dundee.ac.uk
+ * @since 5.0
+ */
+public class ProcessCreator {
+
+    public interface DirectoryMaker {
+
+        void makeDir(String path, boolean parents, Current __current)
+                throws ServerError;
+
+    }
+
+    public interface ProcessState {
+        Ice.Current getCurrent();
+    }
+
+    private final static Log log = LogFactory.getLog(ProcessCreator.class);
+
+    /**
+     * Fields used in date-time calculations.
+     */
+    private static final DateFormatSymbols DATE_FORMAT = new DateFormatSymbols();
+
+    private final String template;
+
+    private final RepositoryDao repositoryDao;
+
+    /**
+     * Creates a {@link Process} instance which can be stored in the
+     * {@link ProcessCreator}.
+     */
+    public ProcessCreator(String template,
+            RepositoryDao repositoryDao) {
+        this.template = template;
+        this.repositoryDao = repositoryDao;
+        log.info("Repository template: " + this.template);
+    }
+
+    /**
+     */
+    public ProcessContainer.Process createProcess(DirectoryMaker dirMaker,
+            List<FsFile> fsFiles,
+            Ice.Current __current) throws ServerError {
+
+        if (fsFiles == null || fsFiles.size() < 1) {
+            throw new omero.ApiUsageException(null, null, "No paths provided");
+        }
+
+        // This is the first part of the string which comes after:
+        // ManagedRepository/, e.g. %user%/%year%/etc.
+        FsFile relPath = new FsFile(expandTemplate(__current));
+        // at this point, relPath should not yet exist on the filesystem
+        createTemplateDir(dirMaker, relPath, __current);
+
+        // The next part of the string which is chosen by the user:
+        // /home/bob/myStuff
+        FsFile basePath = commonRoot(fsFiles);
+
+        // If any two files clash in that chosen basePath directory, then
+        // we'' want to suggest a similar alternative.
+        return newProcess(relPath, basePath, fsFiles, __current);
+    }
+
+    protected ProcessContainer.Process newProcess(FsFile relPath, FsFile basePath,
+            List<FsFile> fsFiles, Ice.Current __current) throws ServerError {
+        return new ManagedImportProcessI(null, relPath, basePath, fsFiles, __current);
+    }
+
+    /**
+     * From a list of paths, calculate the common root path that all share. In
+     * the worst case, that may be "/". May not include the last element, the filename.
+     * @param some paths
+     * @return the paths' common root
+     */
+    protected FsFile commonRoot(List<FsFile> paths) {
+        final List<String> commonRoot = new ArrayList<String>();
+        int index = 0;
+        boolean isCommon = false;
+        while (true) {
+            String component = null;
+            for (final FsFile path : paths) {
+                final List<String> components = path.getComponents();
+                if (components.size() <= index + 1)  // prohibit very last component
+                    isCommon = false;  // not long enough
+                else if (component == null) {
+                    component = components.get(index);
+                    isCommon = true;  // first path
+                } else  // subsequent long-enough path
+                    isCommon = component.equals(components.get(index));
+                if (!isCommon)
+                    break;
+            }
+            if (isCommon)
+                commonRoot.add(paths.get(0).getComponents().get(index++));
+            else
+                break;
+        }
+        return new FsFile(commonRoot);
+    }
+
+    /**
+     * Turn the current template into a relative path. Makes use of the data
+     * returned by {@link #replacementMap(Ice.Current)}.
+     *
+     * @param curr
+     * @return
+     */
+    protected String expandTemplate(Ice.Current curr) {
+
+        if (template == null) {
+            return ""; // EARLY EXIT.
+        }
+
+        final Map<String, String> map = replacementMap(curr);
+        final StrSubstitutor strSubstitutor = new StrSubstitutor(
+                new StrLookup() {
+                    @Override
+                    public String lookup(final String key) {
+                        return map.get(key);
+                    }
+                }, "%", "%", '%');
+        return strSubstitutor.replace(template);
+    }
+
+    /**
+     * Generates a map with most of the fields (as strings) from the
+     * {@link EventContext} for the current user as well as fields from
+     * a current {@link Calendar} instance. Implementors need only
+     * provide the fields that are used in their templates. Any keys that
+     * cannot be found by {@link #expandeTemplate(String, Ice.Current)} will
+     * remain untouched.
+     *
+     * @param curr
+     * @return
+     */
+    protected Map<String, String> replacementMap(Ice.Current curr) {
+        final EventContext ec = this.repositoryDao.getEventContext(curr);
+        final Map<String, String> map = new HashMap<String, String>();
+        final Calendar now = Calendar.getInstance();
+        map.put("user", ec.userName);
+        map.put("userId", Long.toString(ec.userId));
+        map.put("group", ec.groupName);
+        map.put("groupId", Long.toString(ec.groupId));
+        map.put("year", Integer.toString(now.get(Calendar.YEAR)));
+        map.put("month", String.format("%02d", now.get(Calendar.MONTH)+1));
+        map.put("monthname", DATE_FORMAT.getMonths()[now.get(Calendar.MONTH)]);
+        map.put("day", String.format("%02d", now.get(Calendar.DAY_OF_MONTH)));
+        map.put("time", String.format("%02d-%02d-%02d.%03d",
+                now.get(Calendar.HOUR_OF_DAY), now.get(Calendar.MINUTE), now.get(Calendar.SECOND), now.get(Calendar.MILLISECOND)));
+        map.put("session", ec.sessionUuid);
+        map.put("sessionId", Long.toString(ec.sessionId));
+        map.put("eventId", Long.toString(ec.eventId));
+        map.put("perms", ec.groupPermissions.toString());
+        return map;
+    }
+
+    /**
+     * Take the relative path created by
+     * {@link #expandTemplate(String, Ice.Current)} and call
+     * {@link makeDir(String, boolean, Ice.Current)} on each element of the path
+     * starting at the top, until all the directories have been created.
+     * The full path must not already exist, although a prefix of it may.
+     */
+    protected void createTemplateDir(DirectoryMaker dirMaker, FsFile relPath,
+            Ice.Current curr) throws ServerError {
+
+        final List<String> relPathComponents = relPath.getComponents();
+        final int relPathSize = relPathComponents.size();
+        if (relPathSize == 0)
+            throw new IllegalArgumentException("no template directory");
+        if (relPathSize > 1) {
+            final List<String> pathPrefix = relPathComponents.subList(0, relPathSize - 1);
+            dirMaker.makeDir(new FsFile(pathPrefix).toString(), true, curr);
+        }
+        dirMaker.makeDir(relPath.toString(), false, curr);
+    }
+
+    protected String getUserDirectoryName(Current __current) {
+        EventContext ec = repositoryDao.getEventContext(__current);
+        return String.format("%s_%s", ec.userName, ec.userId);
+    }
+
+}
---
+++ components/blitz/test/ome/services/blitz/test/utests/ProcessCreatorTest.java
@@ -0,0 +1,269 @@
+package ome.services.blitz.test.utests;
+
+import java.io.File;
+import java.text.DateFormatSymbols;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import junit.framework.Assert;
+import loci.formats.FormatReader;
+import ome.services.blitz.fire.Registry;
+import ome.services.blitz.repo.FileMaker;
+import ome.services.blitz.repo.ManagedRepositoryI;
+import ome.services.blitz.repo.ProcessCreator;
+import ome.services.blitz.repo.RepositoryDao;
+import ome.services.blitz.repo.path.FsFile;
+import omero.grid.ImportLocation;
+import omero.model.ChecksumAlgorithm;
+import omero.model.PermissionsI;
+import omero.sys.EventContext;
+import omero.util.TempFileManager;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import Ice.Current;
+
+import com.google.common.collect.Sets;
+
+@Test(groups = {"fs"})
+public class ProcessCreatorTest extends MockObjectTestCase {
+
+    Mock daoMock;
+
+    /**
+     * The temporary directory which is equivalent to /OMERO/ManagedRepository
+     */
+    File tmpDir;
+
+    /**
+     * The "expanded" template directory which here is mocked to simply
+     * "template". This should be used when touch()-ing files under
+     * tmpDir.
+     */
+    File templateDir;
+
+    TestProcessCreator creator;
+
+    Ice.Current curr;
+
+    /**
+     * Calendar used to represent "now" in the queries.
+     */
+    Calendar cal;
+
+    public class TestProcessCreator extends ProcessCreator {
+
+        public TestProcessCreator(String template, RepositoryDao repositoryDao) {
+            super(template, repositoryDao);
+        }
+
+        @Override
+        public FsFile commonRoot(List<FsFile> paths) {
+            return super.commonRoot(paths);
+        }
+
+        @Override
+        public String expandTemplate(Current curr) {
+            return super.expandTemplate(curr);
+        }
+    }
+
+    @BeforeMethod(alwaysRun=true)
+    public void setup() throws Exception {
+        this.cal = Calendar.getInstance();
+        this.tmpDir = TempFileManager.create_path("repo", "test", true);
+        this.templateDir = new File(this.tmpDir, "template");
+        this.daoMock = mock(RepositoryDao.class);
+        this.curr = new Ice.Current();
+        this.curr.ctx = new HashMap<String, String>();
+        this.curr.ctx.put(omero.constants.SESSIONUUID.value, "TEST");
+    }
+
+    private void newCreator(String template) {
+        this.creator = new TestProcessCreator(template,
+                (RepositoryDao) daoMock.proxy());
+    }
+
+    private EventContext newEventContext() {
+        EventContext ec = new EventContext();
+        ec.userName = "";
+        ec.userId = -1L;
+        ec.groupName = "";
+        ec.groupId = -1L;
+        ec.sessionUuid = "";
+        ec.sessionId = -1L;
+        ec.eventId = -1L;
+        ec.groupPermissions = new PermissionsI();
+        this.daoMock.expects(atLeastOnce()).method("getEventContext")
+            .with(ANYTHING).will(returnValue(ec));
+        return ec;
+    }
+
+    private static List<FsFile> toFsFileList(String... paths) {
+        final List<FsFile> fsFiles = new ArrayList<FsFile>(paths.length);
+        for (final String path : paths)
+            fsFiles.add(new FsFile(path));
+        return fsFiles;
+    }
+
+    @Test
+    public void testCommonRootReturnsTopLevelWithUncommonPaths() {
+        newCreator("ignore");
+        FsFile expectedCommonRoot = new FsFile();
+        FsFile actualCommonRoot = this.creator.commonRoot(
+                toFsFileList("/home/bob/1.jpg", "/data/alice/1.jpg"));
+        Assert.assertEquals(expectedCommonRoot, actualCommonRoot);
+    }
+
+    @Test
+    public void testCommonRootReturnsCommonRootForPathList() {
+        newCreator("ignore");
+        FsFile expectedCommonRoot = new FsFile("/bob/files/dv");
+        FsFile actualCommonRoot = this.creator.commonRoot(toFsFileList(
+                expectedCommonRoot + "/file1.dv",
+                expectedCommonRoot + "/file2.dv"));
+        Assert.assertEquals(expectedCommonRoot, actualCommonRoot);
+    }
+
+    @Test
+    public void testExpandTemplateEmptyStringOnNullToken() {
+        newCreator(null);
+        newEventContext();
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(0, actual.length());
+    }
+
+    @Test
+    public void testExpandTemplateTokenOnMalformedToken() {
+        newEventContext();
+        String expected = "foo";
+        newCreator(expected);
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateYear() {
+        newCreator("%year%");
+        newEventContext();
+        String expected = Integer.toString(cal.get(Calendar.YEAR));
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateMonth() {
+        newCreator("%month%");
+        newEventContext();
+        String expected = Integer.toString(cal.get(Calendar.MONTH)+1);
+        if (expected.length() == 1)
+            expected = '0' + expected;
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateMonthName() {
+        newCreator("%monthname%");
+        newEventContext();
+        DateFormatSymbols dateFormat = new DateFormatSymbols();
+        String expected = dateFormat.getMonths()
+                [cal.get(Calendar.MONTH)];
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateDay() {
+        newCreator("%day%");
+        newEventContext();
+        String expected = String.format("%02d", cal.get(Calendar.DAY_OF_MONTH));
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateUserName() {
+        newCreator("%user%");
+        String expected = "user-1";
+        EventContext ecStub = newEventContext();
+        ecStub.userName = expected;
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateGroupName() {
+        newCreator("%group%");
+        String expected = "group-1";
+        EventContext ecStub = newEventContext();
+        ecStub.groupName = expected;
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+    @Test
+    public void testExpandTemplateGroupNamePerms() {
+        newCreator("%group%-%perms%");
+        String expected = "group-1-rwrwrw";
+        EventContext ecStub = newEventContext();
+        ecStub.groupName = "group-1";
+        ecStub.groupPermissions = new PermissionsI("rwrwrw");
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateSession() {
+        newCreator("%session%");
+        String expected = UUID.randomUUID().toString();
+        EventContext ecStub = newEventContext();
+        ecStub.sessionUuid = expected;
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateEscape() {
+        newCreator("%%");
+        String expected = "%%";
+        newEventContext();
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateEscape2() {
+        newCreator("%%-%group%");
+        String expected = "%%-grp";
+        EventContext ecStub = newEventContext();
+        ecStub.groupName = "grp";
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateEscape3() {
+        newCreator("%%george");
+        String expected = "%%george";
+        newEventContext();
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testExpandTemplateUnknown() {
+        String expected = "%björk%";
+        newCreator(expected);
+        newEventContext();
+        String actual = this.creator.expandTemplate(curr);
+        Assert.assertEquals(expected, actual);
+    }
+
+}
