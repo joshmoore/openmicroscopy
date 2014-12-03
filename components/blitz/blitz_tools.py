bf1120b3d705e88e579c83a436fc3312ff28e72d
jmoore <josh@glencoesoftware.com>
2014-12-03 11:19:08 +0100

    Don't generate ties for model objects

--- components/blitz/blitz_tools.py
+++ components/blitz/blitz_tools.py
@@ -96,9 +96,12 @@ def make_slice(command):
     return slice
 
 def slice_java(env, where, dir):
-    command  = [slice2java, "--tie"] + common()
     actions = []
     for basename, filename in basenames(where, dir):
+        if "model" not in filename:
+            command  = [slice2java, "--tie"] + common()
+        else:
+            command  = [slice2java] + common()
         c = env.Command(
             jdep(env["DEPMAP"], filename + '.java' ),             # target
             filename + '.ice',                                    # source
