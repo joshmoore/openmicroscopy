a6cad43e31438645dd0ea0002cbb815c0eb1a186
jmoore <josh@glencoesoftware.com>
2014-12-09 11:34:48 +0100

    Add constraint on unit fields (BROKEN)
    Since the two columns `${field}` and `${field}unit`
    are assumed to go together, a constraint has been
    added to prevent nulling just one. Hibernate will
    throw an exception on **loading** a unit where
    the "both null or not null" doesn't hold, so it's
    certainly in our best interest to encode that in
    the DB.

--- sql/psql/OMERO5.1DEV__12/psql-footer.sql
+++ sql/psql/OMERO5.1DEV__12/psql-footer.sql
@@ -2613,6 +2613,42 @@ after delete on originalfile
     for each row execute procedure _fs_log_delete();
 
 
+-- Add constraints to keep value and unit in sync
+
+ALTER TABLE DetectorSettings ADD CONSTRAINT ReadOutRate_nulls check ((ReadOutRate is null and ReadOutRateunit is null) or (ReadOutRate is not null and ReadOutRateunit is not null));
+ALTER TABLE Laser ADD CONSTRAINT RepetitionRate_nulls check ((RepetitionRate is null and RepetitionRateunit is null) or (RepetitionRate is not null and RepetitionRateunit is not null));
+ALTER TABLE Plane ADD CONSTRAINT PositionX_nulls check ((PositionX is null and PositionXunit is null) or (PositionX is not null and PositionXunit is not null));
+ALTER TABLE Plane ADD CONSTRAINT PositionZ_nulls check ((PositionZ is null and PositionZunit is null) or (PositionZ is not null and PositionZunit is not null));
+ALTER TABLE Plane ADD CONSTRAINT PositionY_nulls check ((PositionY is null and PositionYunit is null) or (PositionY is not null and PositionYunit is not null));
+ALTER TABLE Shape ADD CONSTRAINT StrokeWidth_nulls check ((StrokeWidth is null and StrokeWidthunit is null) or (StrokeWidth is not null and StrokeWidthunit is not null));
+ALTER TABLE Shape ADD CONSTRAINT FontSize_nulls check ((FontSize is null and FontSizeunit is null) or (FontSize is not null and FontSizeunit is not null));
+ALTER TABLE LightSourceSettings ADD CONSTRAINT Wavelength_nulls check ((Wavelength is null and Wavelengthunit is null) or (Wavelength is not null and Wavelengthunit is not null));
+ALTER TABLE Plate ADD CONSTRAINT WellOriginX_nulls check ((WellOriginX is null and WellOriginXunit is null) or (WellOriginX is not null and WellOriginXunit is not null));
+ALTER TABLE Plate ADD CONSTRAINT WellOriginY_nulls check ((WellOriginY is null and WellOriginYunit is null) or (WellOriginY is not null and WellOriginYunit is not null));
+ALTER TABLE Objective ADD CONSTRAINT WorkingDistance_nulls check ((WorkingDistance is null and WorkingDistanceunit is null) or (WorkingDistance is not null and WorkingDistanceunit is not null));
+ALTER TABLE Pixels ADD CONSTRAINT PhysicalSizeX_nulls check ((PhysicalSizeX is null and PhysicalSizeXunit is null) or (PhysicalSizeX is not null and PhysicalSizeXunit is not null));
+ALTER TABLE Pixels ADD CONSTRAINT PhysicalSizeZ_nulls check ((PhysicalSizeZ is null and PhysicalSizeZunit is null) or (PhysicalSizeZ is not null and PhysicalSizeZunit is not null));
+ALTER TABLE Pixels ADD CONSTRAINT PhysicalSizeY_nulls check ((PhysicalSizeY is null and PhysicalSizeYunit is null) or (PhysicalSizeY is not null and PhysicalSizeYunit is not null));
+ALTER TABLE StageLabel ADD CONSTRAINT Z_nulls check ((Z is null and Zunit is null) or (Z is not null and Zunit is not null));
+ALTER TABLE StageLabel ADD CONSTRAINT Y_nulls check ((Y is null and Yunit is null) or (Y is not null and Yunit is not null));
+ALTER TABLE StageLabel ADD CONSTRAINT X_nulls check ((X is null and Xunit is null) or (X is not null and Xunit is not null));
+ALTER TABLE WellSample ADD CONSTRAINT PositionX_nulls check ((PositionX is null and PositionXunit is null) or (PositionX is not null and PositionXunit is not null));
+ALTER TABLE WellSample ADD CONSTRAINT PositionY_nulls check ((PositionY is null and PositionYunit is null) or (PositionY is not null and PositionYunit is not null));
+ALTER TABLE Channel ADD CONSTRAINT EmissionWavelength_nulls check ((EmissionWavelength is null and EmissionWavelengthunit is null) or (EmissionWavelength is not null and EmissionWavelengthunit is not null));
+ALTER TABLE Channel ADD CONSTRAINT PinholeSize_nulls check ((PinholeSize is null and PinholeSizeunit is null) or (PinholeSize is not null and PinholeSizeunit is not null));
+ALTER TABLE Channel ADD CONSTRAINT ExcitationWavelength_nulls check ((ExcitationWavelength is null and ExcitationWavelengthunit is null) or (ExcitationWavelength is not null and ExcitationWavelengthunit is not null));
+ALTER TABLE TransmittanceRange ADD CONSTRAINT CutOutTolerance_nulls check ((CutOutTolerance is null and CutOutToleranceunit is null) or (CutOutTolerance is not null and CutOutToleranceunit is not null));
+ALTER TABLE TransmittanceRange ADD CONSTRAINT CutInTolerance_nulls check ((CutInTolerance is null and CutInToleranceunit is null) or (CutInTolerance is not null and CutInToleranceunit is not null));
+ALTER TABLE TransmittanceRange ADD CONSTRAINT CutOut_nulls check ((CutOut is null and CutOutunit is null) or (CutOut is not null and CutOutunit is not null));
+ALTER TABLE TransmittanceRange ADD CONSTRAINT CutIn_nulls check ((CutIn is null and CutInunit is null) or (CutIn is not null and CutInunit is not null));
+ALTER TABLE Laser ADD CONSTRAINT Wavelength_nulls check ((Wavelength is null and Wavelengthunit is null) or (Wavelength is not null and Wavelengthunit is not null));
+ALTER TABLE LightSource ADD CONSTRAINT Power_nulls check ((Power is null and Powerunit is null) or (Power is not null and Powerunit is not null));
+ALTER TABLE ImagingEnvironment ADD CONSTRAINT AirPressure_nulls check ((AirPressure is null and AirPressureunit is null) or (AirPressure is not null and AirPressureunit is not null));
+ALTER TABLE ImagingEnvironment ADD CONSTRAINT Temperature_nulls check ((Temperature is null and Temperatureunit is null) or (Temperature is not null and Temperatureunit is not null));
+ALTER TABLE Plane ADD CONSTRAINT DeltaT_nulls check ((DeltaT is null and DeltaTunit is null) or (DeltaT is not null and DeltaTunit is not null));
+ALTER TABLE Plane ADD CONSTRAINT ExposureTime_nulls check ((ExposureTime is null and ExposureTimeunit is null) or (ExposureTime is not null and ExposureTimeunit is not null));
+ALTER TABLE Pixels ADD CONSTRAINT TimeIncrement_nulls check ((TimeIncrement is null and TimeIncrementunit is null) or (TimeIncrement is not null and TimeIncrementunit is not null));
+
 -- Here we have finished initializing this database.
 update dbpatch set message = 'Database ready.', finished = clock_timestamp()
   where currentVersion = 'OMERO5.1DEV' and
